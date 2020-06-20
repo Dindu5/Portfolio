2638f320896c3106651eeb9ac36dce95f62ea948
Dindu5 <chimdindue@gmail.com>
2020-06-20 11:50:08 +0100

    Updates

---
+++ js/chatbot.js
@@ -0,0 +1,8 @@
+(function(w, d) {
+	w.CollectId = '5eed73e41eb3964b6e7c621e';
+	var h = d.head || d.getElementsByTagName('head')[0];
+	s = d.createElement('script');
+	s.setAttribute('type', 'text/javascript');
+	s.setAttribute('src', 'https://collectcdn.com/launcher.js');
+	h.appendChild(s);
+})(window, document);
